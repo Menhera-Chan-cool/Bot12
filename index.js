92b97df30eb378b6e0b1b611e580f3908a1a099d
Menhera-Chan-cool <69423627+Menhera-Chan-cool@users.noreply.github.com>
2020-09-06 11:09:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const loadFeatures = require('@root/features/load-features')
 const command = require('@util/command')
 
 client.on('ready', async () => {
-  console.log('The client is ready!')
+  console.log(`Bot: Hosting ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
+  client.user.setActivity(`Use ~help.`)
 
   poll(client)
   memberCount(client)
@@ -29,18 +30,6 @@ client.on('ready', async () => {
       message.channel.send(
         `${guild.name} has a total of ${guild.memberCount} members`
       )
-    })
-  })
-
-  command(client, 'status', (message) => {
-    const content = message.content.replace('!status ', '')
-    // "!status hello world" -> "hello world"
-
-    client.user.setPresence({
-      activity: {
-        name: content,
-        type: 0,
-      },
     })
   })
 
