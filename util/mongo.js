2fc014122467564571b7ff0f1b17f773dd9300f6
Menhera-Chan-cool <69423627+Menhera-Chan-cool@users.noreply.github.com>
2020-09-06 11:36:51 +0700

    Add files via upload

--- util/mongo.js
+++ util/mongo.js
@@ -1,6 +1,8 @@
   
 const mongoose = require('mongoose')
-const { mongoPath } = require('@root/config.json')
+//const { mongoPath } = require('@root/config.json')
+
+const mongoPath = 'mongodb+srv://HAHA:loEvrZveHVKebFmS@cluster0.4hdv6.mongodb.net/discordbot?retryWrites=true&w=majority'
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
