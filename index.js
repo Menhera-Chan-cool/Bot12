a9afbb882238d54bcd56260e704c21e3f7baaf95
Menhera-Chan-cool <69423627+Menhera-Chan-cool@users.noreply.github.com>
2020-09-06 11:18:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const client = new Discord.Client()
 const poll = require('@features/poll')
 const memberCount = require('@features/member-count')
 const welcome = require('@features/welcome')
-const mute = require('@features/mute')
 const loadCommands = require('@root/commands/load-commands')
 const commandBase = require('@root/commands/command-base')
 const { loadLanguages } = require('@util/language')
 const loadFeatures = require('@root/features/load-features')
 const command = require('@util/command')
+const mute = require('@features/mute')
 
 client.on('ready', async () => {
-  console.log(`Bot: Hosting ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
-  client.user.setActivity(`Use ~help.`)
+  console.log(`Bot is Online Now!`)
+  client.user.setActivity('Jojo\'s Adventure Is Bizarre', { type: 'PLAYING' })
 
   poll(client)
   memberCount(client)
   welcome(client)
-  mute(client)
   loadCommands(client)
   commandBase.loadPrefixes(client)
   loadLanguages(client)
   loadFeatures(client)
+  mute(client)
 
   command(client, 'membercount', (message) => {
     client.guilds.cache.forEach((guild) => {
